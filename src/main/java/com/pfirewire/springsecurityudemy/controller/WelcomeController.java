af547a0876fa0c2e4d6ba9067ec9ef7212001e14
Stephen Pflug <stephen@pflug.us>
2022-11-01 11:17:46 -0500

    created basic rest application and added spring security to dependencies

---
+++ src/main/java/com/pfirewire/springsecurityudemy/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.pfirewire.springsecurityudemy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to Spring Application WITH Security";
+    }
+}
