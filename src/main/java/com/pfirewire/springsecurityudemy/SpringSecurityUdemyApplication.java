2f8967a55e9da0619480ec2ac0619e9be3e60429
Stephen Pflug <stephen@pflug.us>
2022-11-01 10:59:16 -0500

    first commit

---
+++ src/main/java/com/pfirewire/springsecurityudemy/SpringSecurityUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.pfirewire.springsecurityudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pfirewire/springsecurityudemy/SpringSecurityUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfirewire.springsecurityudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
